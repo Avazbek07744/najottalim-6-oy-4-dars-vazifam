c54eb70bc5419ef672d228ddceb0a844f8bb3c39
Avazbek07744 <javazbek87@gmail.com>
2024-09-20 22:24:48 +0500

    vazifam

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -14,11 +14,11 @@ const Card = ({ data, onDelete }) => {
     return (
         <div>
             <div className="card border border-[aqua] w-[250px] p-3 rounded-xl">
-                <h2 className='text-[aqua] font-bold text-2xl mb-1 capitalize'>username: {username}</h2>
-                <h3 className='text-[aqua] font-bold text-xl mb-1 capitalize'>email: {email}</h3>
-                <h3 className='text-[aqua] font-bold text-xl mb-1 capitalize'>number: {number}</h3>
-                <h3 className='text-[aqua] font-bold text-xl mb-1 capitalize'>nationality: {nat}</h3>
-                <h3 className='text-[aqua] font-bold text-xl mb-1 capitalize'>description: {desc}</h3>
+                <h2 className='text-[aqua] font-bold text-2xl mb-1'>username: {username}</h2>
+                <h3 className='text-[aqua] font-bold text-xl mb-1'>email: {email}</h3>
+                <h3 className='text-[aqua] font-bold text-xl mb-1'>number: {number}</h3>
+                <h3 className='text-[aqua] font-bold text-xl mb-1'>nationality: {nat}</h3>
+                <h3 className='text-[aqua] font-bold text-xl mb-1'>description: {desc}</h3>
                 <button onClick={handleDeleteClick} className='border border-[aqua] px-3 py-1 text-xl text-[aqua] font-bold rounded-xl mt-5 hov'>Delete</button>
             </div>
         </div>
